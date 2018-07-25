3b6fc67b68566d3208d75c2005179229a939b835
Jacob Runyan <jmrunyan@ucsc.edu>
2018-07-25 15:48:49 -0700

    Initial Scraping works well enough to continue. Working on storing the data into a database.

--- headlesschrometesting/headlesschrome.py
+++ headlesschrometesting/headlesschrome.py
@@ -1,4 +1,13 @@
+# https://github.com/runyanjake/cryptogoochers/headlesschrometesting/headlesschrome.py
+# @Author Jake Runyan
+# Testing of headless chrome to scrape cryptocoin data and store it in a database, generating a nice graph as output.
+# Running this script performs one complete scrape of relevant data (enough to fully populate a database row), generates a graph and exits.
+# It is designed to be called on a timetable for continuous data acquisition.
+#
+
 import os 
+import re
+import sqlite3
 import time
 from selenium import webdriver  
 from selenium.webdriver.chrome.options import Options  
@@ -7,30 +16,40 @@ from selenium.webdriver.common.keys import Keys
 PATH_TO_CHROMEDRIVER = './chromedriver/chromedriver'
 driver = webdriver.Chrome(PATH_TO_CHROMEDRIVER)  # Chrome driver, using the chromedriver file in PATH_TO_CHROMEDRIVER, if not specified will search path.
 
-CRYPTO_WEB_SOURCES = ['https://www.coindesk.com',
-                      'https://cointelegraph.com/bitcoin-price-index']
+BTC_WEB_SOURCES = ['https://www.coindesk.com/price/',
+                        'https://cointelegraph.com/bitcoin-price-index']
+BTC_PRICE_XPATHS = ["//span[@class='data']",
+                        "//div[@class='value text-nowrap']"]
+BTC_PRICES = []
 
-# driver.get('http://www.google.com/xhtml')
-# time.sleep(5) # Let the user actually see something!
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# time.sleep(5) # Let the user actually see something!
+assert(len(BTC_WEB_SOURCES) == len(BTC_PRICE_XPATHS)) #the 2 correspond for links and scraping commands
 
-driver.get(CRYPTO_WEB_SOURCES[1])
-btc_value_element = driver.find_element_by_xpath("//div[@class='value text-nowrap']")
-btc_value = btc_value_element.text
-print("BTC VALUE: " + str(btc_value))
+#Scraping Prices
+for itor in range(0, len(BTC_WEB_SOURCES)):
+    succeeded_scraping = False
+    num_reattempts = 10
+    while not succeeded_scraping:
+        print("Scraping " + str(BTC_WEB_SOURCES[itor]) + "...")
+        driver.get(BTC_WEB_SOURCES[itor])
+        btc_value_element = driver.find_element_by_xpath(BTC_PRICE_XPATHS[itor])
+        btc_value_str = btc_value_element.text
+        if btc_value_str == "":
+            print("Failed to parse a string from the webpage. Retrying... (" + str(num_reattempts) + " attempts left)")
+            num_reattempts = num_reattempts-1
+            if num_reattempts > 0:
+                succeeded_scraping = False
+            else:
+                print("Failed to parse a string from the webpage. Continuing...")
+                btc_value = -1.0 #error value, most likely never used
+                succeeded_scraping = True #loop control, dont mean anything
+        else:
+            btc_value = float(re.search("[0-9,]+\.[0-9]+", btc_value_str).group(0).replace(",", ""))
+            BTC_PRICES.append(btc_value)
+            print("Found BTC price " + str(btc_value))
+            succeeded_scraping = True
+            print("Done.")
 
-# for url in CRYPTO_WEB_SOURCES:
-#     print("REPORT FROM " + url + ": ")
-#     driver.get(url)
-#     try:
-#         coin_name = driver.find_element_by_xpath('//div[@class=value text-nowrap]').text
-#         coin_val = driver.find_element_by_xpath('//div[@class=price-link]').text
-#         print('\t' + str(coin_name) + '\'s price is ' + str(coin_val))
-#     except AttributeError:
-#         print("\tWas not found on this page.")
-#     time.sleep(2)
+#Storing Prices
+if len(BTC_PRICES) > 0
 
 driver.quit()
