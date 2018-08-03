5380225392603424074177c2f16dbb7bd174747f
Jacob Runyan <jmrunyan@ucsc.edu>
2018-08-03 01:23:09 -0700

    Got the pickling code from sentdex to work! it actually works thats crazy

--- sentimentanalysis/sentimentanalysis.py
+++ sentimentanalysis/sentimentanalysis.py
@@ -6,7 +6,7 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import json
-# import sentiment_mod as s
+import sentiment_mod as s
 
 #consumer key, consumer secret, access token, access secret.
 ckey="WsN6anOVvQaaUOSVQ0aDMimUj"
@@ -20,17 +20,16 @@ class listener(StreamListener):
         all_data = json.loads(data)
         
         tweet = all_data["text"]
-        print(tweet)
 
-        # sentiment_value, confidence = s.sentiment(tweet)
+        sentiment_value, confidence = s.sentiment(tweet)
 
-        # print(tweet, sentiment_value, confidence)
+        print(tweet, sentiment_value, confidence)
 
-        # if confidence*100 >= 80:
-        #     output = open("twitterout.txt", "a")
-        #     output.write(sentiment_value)
-        #     output.write("\n")
-        #     output.close()
+        if confidence*100 >= 80:
+            output = open("twitterout.txt", "a")
+            output.write(sentiment_value + tweet)
+            output.write("\n")
+            output.close()
         
         return True
 
