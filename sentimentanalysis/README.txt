d3dab9a566d6c331eedd0e55bcdd87d367d72be9
Jacob Runyan <jmrunyan@ucsc.edu>
2018-08-03 00:18:39 -0700

    Added sentimentanalysis folder

---
+++ sentimentanalysis/README.txt
@@ -0,0 +1,12 @@
+Install tweepy: pip install tweepy
+
+Get the twitter ckey at https://apps.twitter.com
+
+virtualenv -p python3 env     	//create an environment called env
+source env/bin/activate	       	//start the environment
+source deactivate		//close the environment
+
+pip install matplotlib
+pip install selenium		//the selenium package for running headless browsers
+pip install plotly
+pip install plotly --upgrade
---
+++ sentimentanalysis/sentimentanalysis.py
@@ -0,0 +1,44 @@
+# @author Jake Runyan
+# sentimentanalysis.py
+# Following along with sentdex's twitter sentiment analysis tutorial on youtube.
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+# import sentiment_mod as s
+
+#consumer key, consumer secret, access token, access secret.
+ckey="WsN6anOVvQaaUOSVQ0aDMimUj"
+csecret="5Wx6R4OZRQh1rNzyq8FePXiO0Uo9yuz0fggGJqMnqxK4ibI0Ip"
+atoken="1025266565619445760-IA52p8ZOqx2DHxydyXCYMkL4gmtEsn"
+asecret="TPNzK9xgUjUQkpUWk9aFRJ3xx8z8XyXenUxLJJfWI7K2L"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+        
+        tweet = all_data["text"]
+        print(tweet)
+
+        # sentiment_value, confidence = s.sentiment(tweet)
+
+        # print(tweet, sentiment_value, confidence)
+
+        # if confidence*100 >= 80:
+        #     output = open("twitterout.txt", "a")
+        #     output.write(sentiment_value)
+        #     output.write("\n")
+        #     output.close()
+        
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["car"])
