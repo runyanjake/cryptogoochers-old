6e63af43f4f1703d938089daaac81f4e88753335
Jacob Runyan <jmrunyan@ucsc.edu>
2018-09-08 15:56:40 -0700

    Renamed to JScraper.py. Implemented the sqlite and json initializer methods.

---
+++ headlessbrowserscraping/JScraper.py
@@ -0,0 +1,76 @@
+# @author Jake Runyan
+# @desc A "Jscraper" web scraper ADT that runs a selenium instance headless or headed to drive chrome or firefox.
+
+import json
+import sqlite3
+import sys
+
+class Target:
+    ticker = ""
+    site = ""
+    xpath = ""
+    def __init__(self, ttik, turl, tpath):
+        self.ticker = ttik
+        self.url = turl
+        self.xpath = tpath
+    def __str__(self):
+        return ("{" + str(self.ticker) + " | " + str(self.url) + " | " + str(self.xpath) + "}")
+
+class JScraperException(Exception):
+    pass
+
+class JScraper:
+    #private
+    __currencyTypes = []
+    __targets = None
+
+    def __loadTargets(self, file): #load scrape locations from json as array of Targets
+        data = None
+        self.__targets = []
+        try:
+            with open(file, "r") as jsonfile:
+                data = json.load(jsonfile)
+            for currency in data:
+                self.__currencyTypes.append(currency)
+                for entry in data[currency]:
+                    self.__targets.append(Target(currency, entry["url"], entry["xpath"]))
+        except(IOError):
+            raise JScraperException("Specified JSON file " + str(file) + " does not exist.")
+
+    def __loadDatabase(self, dtbfile):
+        #requires targets to be loaded so currencyTypes exists.
+        connection = sqlite3.connect(dtbfile)
+        for currency in self.__currencyTypes:
+            tablename = currency + "prices"
+            try:
+                connection.execute('''CREATE TABLE ''' + str(tablename) + 
+                        ''' (currency text, date timestamp, median_price real, hi_price real, hi_price_site text, lo_price real, lo_price_site text)''')
+            except sqlite3.OperationalError:
+                pass
+
+    #public
+    def __init__(self, jsonfile="directory.json", dtbfile="./databases/jscraper.db", logfile="log.txt"):
+        self.__loadTargets(jsonfile)
+        self.__loadDatabase(dtbfile)
+
+    def printTargets(self):
+        for target in self.__targets:
+            print(target)
+
+    def printCurrencies(self):
+        sys.stdout.write("Currencies: {")
+        first = True
+        for currency in self.__currencyTypes:
+            if first:
+                sys.stdout.write(currency)
+                first = False
+            else:
+                sys.stdout.write(", " + currency)
+        sys.stdout.write("}\n")
+
+    def scrape(self):
+        pass
+
+scpr = JScraper()
+scpr.printCurrencies()
+scpr.printTargets()
